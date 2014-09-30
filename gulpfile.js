f5829925c05fc266a3fd0605dd8857add5248780
Aaron Burtnyk <burt202@gmail.com>
2014-10-01 00:21:19 +0100

    updates npm dependency versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,22 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	less = require('gulp-less'),
-	minifyCSS = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
 	jeditor = require('gulp-json-editor'),
 	shell = require('gulp-shell'),
 	jasmine = require('gulp-jasmine');
 
-gulp.task('default', function() {
-	gulp.watch('public/css/**', function() {
-		gulp.run('compile-less');
-	});
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function () {
+    gulp.watch('public/css/**', ['compile-less']);
 });
 
-gulp.task('build', function () {
-	gulp.run('bundle-js', 'type-production');
-});
+gulp.task('build', ['compile-less', 'bundle-js', 'type-production']);
 
-gulp.task('unbuild', function () {
-	gulp.run('type-development');
-});
+gulp.task('unbuild', ['type-development']);
 
-gulp.task('jasmine', function() {
+gulp.task('client-tests', function() {
 	gulp.src('tests/**')
 		.pipe(jasmine({
 			verbose: true
@@ -31,9 +26,7 @@ gulp.task('jasmine', function() {
 gulp.task('jshint', function() {
 	gulp.src([
 			'public/js/**',
-			'tests/**',
-			'!public/build/*.js',
-			'!public/js/libs/**'
+			'tests/**'
 		])
 		.pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter('jshint-stylish'));
@@ -42,9 +35,9 @@ gulp.task('jshint', function() {
 gulp.task('compile-less', function() {
 	gulp.src('public/css/imports.less')
 		.pipe(less({
-			paths: ['public/css/']
+			paths: ['public/css/'],
+			compress: true
 		}))
-		.pipe(minifyCSS())
 		.pipe(rename('combined.css'))
 		.pipe(gulp.dest('public/build/'));
 });
@@ -68,5 +61,5 @@ gulp.task('type-development', function() {
 });
 
 gulp.task('bundle-js', shell.task([
-	'node public/build/r.js -o public/build/build.json'
+    'node public/bower_components/rjs/dist/r.js -o public/build/build.json > public/build/build.js.log'
 ]));
